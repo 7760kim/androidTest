37156b8c51f4513777853eef29f892cf376b7c4f
7760k <7760k@DESKTOP-CME283D>
2019-05-03 17:27:13 +0900

    main commit

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -10,14 +10,6 @@ public class Main{
 
 	public static void main(String[] args) throws Exception {
 
-		String a = "1";
-		String b = a;
-		String c= new String("1");
-
-
-		System.out.println(a.equals(b));
-		System.out.println(a == b);
-		System.out.println(a == c);
-		System.out.println(a.equals(c));
+		System.out.println("hello git!!");
 	}
 }
